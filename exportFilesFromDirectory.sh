b7bfcc47cf3d9fe739f5d3beb9844f607faf582a
Lucas Neves <lucas54neves@outlook.com>
2021-02-03 11:46:31 -0300

    Add second version of the script

--- exportFilesFromDirectory.sh
+++ exportFilesFromDirectory.sh
@@ -6,13 +6,13 @@ dictory=$1
 
 dictoryPath=$currentPath/$dictory
 
-filesNamesArray=($1*.ts)
+filesNamesArray=($dictoryPath/*.ts)
 
 for fileName in "${filesNamesArray[@]}"; do
     IFS='.' read -r -a pathsArray <<< $fileName
     IFS='/' read -r -a nameArray <<< ${pathsArray[0]}
 
-    name=${nameArray[1]}
+    name=${nameArray[-1]}
 
-    echo "export * from './"$name"'" >> $dictoryPath'index.ts'
+    echo "export * from './"$name"'" >> $dictoryPath'/index.ts'
 done
